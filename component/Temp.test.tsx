885e71a5e940752c956dc5da1f40f789ff9cf590
fabrigeas <fabrigeas@gmail.com>
2023-02-02 22:27:40 +0100

    style: npm run prettier

--- component/Temp.test.tsx
+++ component/Temp.test.tsx
@@ -4,32 +4,34 @@ import ReactDOM from 'react-dom/client';
 import { act } from 'react-dom/test-utils';
 import Temp from './Temp';
 
+const testId = 'Temp';
+
 let container: HTMLDivElement;
 
 beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
 
 afterEach(() => {
-    document.body.removeChild(container);
-    container.remove();
+  document.body.removeChild(container);
+  container.remove();
 });
 
 it('renders', () => {
-    act(() => {
-        ReactDOM.createRoot(container).render(<Temp />);
-    });
+  act(() => {
+    ReactDOM.createRoot(container).render(<Temp />);
+  });
 
-    expect(screen.getBytestId('Temp')).toBeInTheDocument();
+  expect(screen.getByTestId(testId)).toBeInTheDocument();
 });
 
-describe.each(['lorem', 'dolor', 'ipsum'])('', (title) => {
-    test(`title => ${title}`, async () => {
-        act(() => {
-            render(<Temp title={title} />);
-        });
-
-        expect(screen.getByText(title)).toBeInTheDocument();
+describe.each(['lorem', 'dolor', 'ipsum'])('', title => {
+  test(`title => ${title}`, async () => {
+    act(() => {
+      render(<Temp title={title} />);
     });
+
+    expect(screen.getByTestId(testId)).toBeInTheDocument();
+  });
 });
